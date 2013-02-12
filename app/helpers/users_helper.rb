177dceaede6beb3924005ad48b52f8e0434a6cf0
Matt Slotkin <matthew.slotkin@gmail.com>
2013-02-11 23:18:29 -0500

    Done with user edit/update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
 	def gravatar_for(user, options = {:size => 50})
 		gravatar_image_tag(user.email.downcase, :alt => user.name, :class => "gravatar", :gravatar => options)
 	end
+
+	def button_text
+		if @title == "Edit user"
+			"Update"
+		else
+			"Sign up"
+		end
+	end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
 	factory :user do |user|
 	  user.name					"Matt Slotkin"
-	  user.email					"matt@thetudu.com"
+	  user.email					"fake@fake.com"
 	  user.password				"foobar"
       user.password_confirmation	"foobar"
-  end
+    end
 end
